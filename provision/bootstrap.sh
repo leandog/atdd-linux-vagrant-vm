da53a88b209b8ddfd494dba3cbde13b961c51fa1
Eric Hankinson <eric.hankinson@gmail.com>
2019-04-03 15:10:30 -0400

    Add logging, generate Rails database for Puppies site

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -2,29 +2,33 @@
 
 source ~/.bash_profile
 
+LOG_PATH_AND_NAME="/vagrant/provision-results.log"
+
+echo "Starting Log -----------\n" > $LOG_PATH_AND_NAME
+
 echo -e "\n**** Installing Jetbrains RubyMine IDE ****\n"
 
-RUBYMINE_DOWNLOAD_LINK=https://download.jetbrains.com/ruby/RubyMine-2018.3.4.tar.gz
+RUBYMINE_DOWNLOAD_LINK=https://download.jetbrains.com/ruby/RubyMine-2018.3.5.tar.gz
 RUBYMINE_DOWNLOAD_FILENAME=${RUBYMINE_DOWNLOAD_LINK##*/}
 
 if [[ ! -f ~/$RUBYMINE_DOWNLOAD_FILENAME ]]; then
   echo -e "\n**** Downloading RubyMine ****\n"
-  echo -e "\n**** Downloading RubyMine ****\n" > /dev/null 2>&1
-  curl -Lo ~/$RUBYMINE_DOWNLOAD_FILENAME $RUBYMINE_DOWNLOAD_LINK > /dev/null 2>&1
+  echo -e "\n**** Downloading RubyMine ****\n" >> $LOG_PATH_AND_NAME 2>&1
+  curl -Lo ~/$RUBYMINE_DOWNLOAD_FILENAME $RUBYMINE_DOWNLOAD_LINK >> $LOG_PATH_AND_NAME 2>&1
 
   if [[ ! -f ~/$RUBYMINE_DOWNLOAD_FILENAME ]]; then
     echo -e "\nERROR: unable to download RubyMine \n"
-    echo -e "\nERROR: unable to download RubyMine \n" > /dev/null 2>&1
+    echo -e "\nERROR: unable to download RubyMine \n" >> $LOG_PATH_AND_NAME 2>&1
     exit 0
   fi
 else
   echo -e "\n**** Already have RubyMine ****\n"
-  echo -e "\n**** Already have RubyMine ****\n" > /dev/null 2>&1
+  echo -e "\n**** Already have RubyMine ****\n" >> $LOG_PATH_AND_NAME 2>&1
 fi
 
 echo -e "\n**** Install RubyMine ****\n"
 sudo mkdir -p /opt/RubyMine
-sudo tar -zxvf ~/$RUBYMINE_DOWNLOAD_FILENAME --strip-components 1 -C /opt/RubyMine > /dev/null 2>&1
+sudo tar -zxvf ~/$RUBYMINE_DOWNLOAD_FILENAME --strip-components 1 -C /opt/RubyMine >> $LOG_PATH_AND_NAME 2>&1
 sudo chown -R root:root /opt/RubyMine
 rm ~/$RUBYMINE_DOWNLOAD_FILENAME
 
@@ -41,7 +45,7 @@ Terminal=false
 StartupWMClass=jetbrains-rubymine
 EOF
 
-sudo apt-get -y remove unity-lens-shopping > /dev/null 2>&1
+sudo apt-get -y remove unity-lens-shopping >> $LOG_PATH_AND_NAME 2>&1
 
 echo -e "\n**** Adjusting taskbar default shortcuts ****\n"
 
@@ -62,30 +66,30 @@ echo -e "\n**** Do not install Ruby documentation with gems, most current docs a
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
 echo -e "\n**** Configure default Ruby for Vagrant user ****\n"
-rvm install ruby-2.3 > /dev/null 2>&1
-rvm use ruby-2.3 --default > /dev/null 2>&1
-rvm gemset use global > /dev/null 2>&1
-gem install bundler > /dev/null 2>&1
-gem install neo-git-pair > /dev/null 2>&1
+rvm install ruby-2.3 >> $LOG_PATH_AND_NAME 2>&1
+rvm use ruby-2.3 --default >> $LOG_PATH_AND_NAME 2>&1
+rvm gemset use global >> $LOG_PATH_AND_NAME 2>&1
+gem install bundler >> $LOG_PATH_AND_NAME 2>&1
 
 echo -e "\n**** Configure default NodeJS for Vagrant User ****\n"
-nvm install 6.8.1 > /dev/null 2>&1
-nvm use 6.8.1 > /dev/null 2>&1
-nvm alias default 6.8.1 > /dev/null 2>&1
-npm install -g npm > /dev/null 2>&1
+nvm install 6.8.1 >> $LOG_PATH_AND_NAME 2>&1
+nvm use 6.8.1 >> $LOG_PATH_AND_NAME 2>&1
+nvm alias default 6.8.1 >> $LOG_PATH_AND_NAME 2>&1
+npm install -g npm >> $LOG_PATH_AND_NAME 2>&1
 
 echo -e "\n**** Downloading & setting up Web Test Automation Projects ****\n"
 cd ~/Projects/
-git clone https://github.com/leandog/puppies.git > /dev/null 2>&1
-git clone https://github.com/leandog/atdd-with-puppies.git > /dev/null 2>&1
+git clone https://github.com/leandog/puppies.git >> $LOG_PATH_AND_NAME 2>&1
+git clone https://github.com/leandog/atdd-with-puppies.git >> $LOG_PATH_AND_NAME 2>&1
 
 echo -e "\n**** Installing Ruby Gems for Web Project ****\n"
-cd ~/Projects/puppies > /dev/null 2>&1
-bundle install > /dev/null 2>&1
+cd ~/Projects/puppies >> $LOG_PATH_AND_NAME 2>&1
+bundle install >> $LOG_PATH_AND_NAME 2>&1
+rake db:setup >> $LOG_PATH_AND_NAME 2>&1
 
 echo -e "\n**** Installing Ruby Gems for Test Automation Project ****\n"
-cd ~/Projects/atdd-with-puppies > /dev/null 2>&1
-bundle install > /dev/null 2>&1
+cd ~/Projects/atdd-with-puppies >> $LOG_PATH_AND_NAME 2>&1
+bundle install >> $LOG_PATH_AND_NAME 2>&1
 
 echo -e "\n**** Finished installation & setup! ****\n"
 
